ee98fb23a8faec62e5e81ef7040d20061dd00215
Peter Matthews <peterjmatthews@me.com>
2023-04-18 15:02:38 +0100

    move useState into specific components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import { Review } from "./interfaces/review.interface";
 import ListOfReviews from "./components/ListReviews";
-import Background from "./components/Background";
 
 function App(): JSX.Element {
-  const [reviewList, setReviewList] = useState<Review[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+
 
   return (
     <div className="App">
       <Header />
-      <ListOfReviews
-        isLoading={isLoading}
-        setIsLoading={setIsLoading}
-        reviewList={reviewList}
-        setReviewList={setReviewList}
-      />
+      <ListOfReviews />
     </div>
   );
 }
--- src/components/ListReviews.tsx
+++ src/components/ListReviews.tsx
@@ -1,22 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchReviews } from "../utils/api";
 import { Review } from "../interfaces/review.interface";
 import ReviewCard from "./ReviewCard";
 import LoadingReviewCard from "./LoadingReview";
 
-interface Props {
-  reviewList: Review[];
-  setReviewList: React.Dispatch<React.SetStateAction<Array<Review>>>;
-  isLoading: boolean;
-  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
-}
+// interface Props {
+//   reviewList: Review[];
+//   setReviewList: React.Dispatch<React.SetStateAction<Array<Review>>>;
+//   isLoading: boolean;
+//   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+// }
 
-const ListOfReviews = ({
-  reviewList,
-  setReviewList,
-  isLoading,
-  setIsLoading,
-}: Props): JSX.Element => {
+const ListOfReviews = (): JSX.Element => {
   useEffect(() => {
     setIsLoading(true);
     fetchReviews().then((data: Review[]) => {
@@ -25,16 +20,21 @@ const ListOfReviews = ({
     });
   }, []);
 
+  const [reviewList, setReviewList] = useState<Review[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
     <div>
       <h2 className="page-title">List of Reviews</h2>
-      {/* {isLoading ? <LoadingReviewCard/> : null} */}
-      {isLoading ? <LoadingReviewCard/> : <ul className="list-container">
-        {reviewList.map((review: Review) => {
-          return <ReviewCard review={review} />;
-        })}
-      </ul>}
-      
+      {isLoading ? (
+        <LoadingReviewCard />
+      ) : (
+        <ul className="list-container">
+          {reviewList.map((review: Review) => {
+            return <ReviewCard review={review} />;
+          })}
+        </ul>
+      )}
     </div>
   );
 };
