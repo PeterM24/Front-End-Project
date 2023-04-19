a7aea8d2990495e386d54b18a6d20971ae961c38
Peter Matthews <peterjmatthews@me.com>
2023-04-19 16:17:39 +0100

    initial logic for voting component

--- src/components/ReviewVote.tsx
+++ src/components/ReviewVote.tsx
@@ -1,18 +1,48 @@
+import { useEffect, useState } from "react";
+
 const ReviewVote = ({
-  votes,
-  review_id,
-}: {
-  votes?: number;
-  review_id?: number;
-}): JSX.Element => {
-    
-  return (
-    <div className="vote-buttons">
-      <button>Vote up</button>
-      <button>Vote down</button>
-      <h4>Total votes: {votes}</h4>
-    </div>
-  );
-};
+    votes,
+    review_id,
+  }: {
+    votes?: number;
+    review_id?: number;
+  }): JSX.Element => {
+    const [vote, setVote] = useState<number>();
+    const [hasVoted, setHasVoted] = useState<boolean>(false);
+  
+    const handleVoteUp = () => {
+      if (!hasVoted) {
+        setVote((prevVote) => (prevVote || 0) + 1);
+        setHasVoted(true);
+        // make API call to upvote review
+      }
+    };
+  
+    const handleVoteDown = () => {
+      if (!hasVoted) {
+        setVote((prevVote) => (prevVote || 0) - 1);
+        setHasVoted(true);
+        // make API call to downvote review
+      }
+    };
+  
+    useEffect(() => {
+      // make API call to get current votes
+      setVote(votes);
+    }, [votes]);
+  
+    return (
+      <div className="vote-buttons">
+        <button onClick={handleVoteUp} disabled={hasVoted}>
+          Vote up
+        </button>
+        <button onClick={handleVoteDown} disabled={hasVoted}>
+          Vote down
+        </button>
+        <h4>Total votes: {vote || votes}</h4>
+        {hasVoted && <p>Thank you for your vote!</p>}
+      </div>
+    );
+  };
 
 export default ReviewVote;
