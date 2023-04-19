a92a44c0e28493a1fa76c5d64b120864288326e1
Peter Matthews <peterjmatthews@me.com>
2023-04-19 11:29:18 +0100

    Update CommentsSingleReview: - better structure on page

--- src/components/CommentsSingleReview.tsx
+++ src/components/CommentsSingleReview.tsx
@@ -7,6 +7,7 @@ const CommentsSingleReview = (): JSX.Element => {
   const { review_id } = useParams();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [commentList, setCommentList] = useState<Comment[]>([]);
+  const [date, setDate] = useState<string>("");
 
   useEffect(() => {
     setIsLoading(true);
@@ -17,18 +18,20 @@ const CommentsSingleReview = (): JSX.Element => {
   }, []);
 
   return (
-      <ul className="comments-ul">
-        {
-            commentList.map((comment) => {
-                return <li key={comment.comment_id}>
-                    <h3>{comment.author}</h3>
-                    <p>{comment.body}</p>
-                    <p>{comment.created_at}</p>
-                    <p>{comment.votes}</p>
-                </li>
-            })
-        }
-      </ul>
+    <ul className="comments-ul">
+      <h3>Comments:</h3>
+      {commentList.map((comment) => {
+        const date = new Date(comment.created_at).toLocaleString();
+        return (
+          <li key={comment.comment_id} className="individual-comment">
+            <h3 className="comment-heading">{comment.author}</h3>
+            <p className="comment-date">Posted: {date}</p>
+            <p className="comment-body">{comment.body}</p>
+            <p className="comment-votes">Votes: {comment.votes}</p>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
