6c2d27ef059e36123617b7b45b693c68c5921a75
Peter Matthews <peterjmatthews@me.com>
2023-04-19 16:52:32 +0100

    Complete functionality of voting component: - Vote up can only be done once - Vote down can only be done once - Total votes updates optimistically - Buttons disable - UX message displays telling user success

--- src/components/ReviewVote.tsx
+++ src/components/ReviewVote.tsx
@@ -1,48 +1,50 @@
 import { useEffect, useState } from "react";
+import { fetchSingleReview, patchVotes } from "../utils/api";
 
 const ReviewVote = ({
-    votes,
-    review_id,
-  }: {
-    votes?: number;
-    review_id?: number;
-  }): JSX.Element => {
-    const [vote, setVote] = useState<number>();
-    const [hasVoted, setHasVoted] = useState<boolean>(false);
-  
-    const handleVoteUp = () => {
-      if (!hasVoted) {
-        setVote((prevVote) => (prevVote || 0) + 1);
-        setHasVoted(true);
-        // make API call to upvote review
-      }
-    };
-  
-    const handleVoteDown = () => {
-      if (!hasVoted) {
-        setVote((prevVote) => (prevVote || 0) - 1);
-        setHasVoted(true);
-        // make API call to downvote review
-      }
-    };
-  
-    useEffect(() => {
-      // make API call to get current votes
-      setVote(votes);
-    }, [votes]);
-  
-    return (
-      <div className="vote-buttons">
-        <button onClick={handleVoteUp} disabled={hasVoted}>
-          Vote up
-        </button>
-        <button onClick={handleVoteDown} disabled={hasVoted}>
-          Vote down
-        </button>
-        <h4>Total votes: {vote || votes}</h4>
-        {hasVoted && <p>Thank you for your vote!</p>}
-      </div>
-    );
+  votes: initialVotes,
+  review_id,
+}: {
+  votes?: number;
+  review_id?: any;
+}): JSX.Element => {
+  const [votes, setVotes] = useState<number>(initialVotes || 0);
+  const [voteType, setVoteType] = useState<"up" | "down" | undefined>();
+  const [hasVoted, setHasVoted] = useState<boolean>(false);
+
+  const handleVoteUp = async () => {
+    if (!hasVoted || voteType === "down") {
+      setVotes((prevVotes) => prevVotes + 1);
+      setVoteType("up");
+      setHasVoted(true);
+      await patchVotes(review_id, 1);
+    }
   };
+
+  const handleVoteDown = async () => {
+    if (!hasVoted || voteType === "up") {
+      setVotes((prevVotes) => prevVotes - 1);
+      setVoteType("down");
+      setHasVoted(true);
+      await patchVotes(review_id, -1);
+    }
+  };
+
+  return (
+    <div className="vote-buttons">
+      <button onClick={handleVoteUp} disabled={hasVoted && voteType === "up"}>
+        Vote up
+      </button>
+      <button
+        onClick={handleVoteDown}
+        disabled={hasVoted && voteType === "down"}
+      >
+        Vote down
+      </button>
+      <h4>Total votes: {votes}</h4>
+      {hasVoted && <p>Thank you for your vote!</p>}
+    </div>
+  );
+};
 
 export default ReviewVote;
