59c8e0c291ed156d3e1c20fd664cdf00c42b9e69
Peter Matthews <peterjmatthews@me.com>
2023-04-19 11:46:49 +0100

    Add loading animation for comment fetching

--- src/components/CommentsSingleReview.tsx
+++ src/components/CommentsSingleReview.tsx
@@ -1,3 +1,4 @@
+import "../styles/LoadingDotsDark.css"
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Comment } from "../interfaces/comment.interface";
@@ -18,20 +19,31 @@ const CommentsSingleReview = (): JSX.Element => {
   }, []);
 
   return (
-    <ul className="comments-ul">
-      <h3>Comments:</h3>
-      {commentList.map((comment) => {
-        const date = new Date(comment.created_at).toLocaleString();
-        return (
-          <li key={comment.comment_id} className="individual-comment">
-            <h3 className="comment-heading">{comment.author}</h3>
-            <p className="comment-date">Posted: {date}</p>
-            <p className="comment-body">{comment.body}</p>
-            <p className="comment-votes">Votes: {comment.votes}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {isLoading ? (
+        <div className="lds-ellipsis-dark">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      ) : (
+        <ul className="comments-ul">
+          <h3>Comments:</h3>
+          {commentList.map((comment) => {
+            const date = new Date(comment.created_at).toLocaleString();
+            return (
+              <li key={comment.comment_id} className="individual-comment">
+                <h3 className="comment-heading">{comment.author}</h3>
+                <p className="comment-date">Posted: {date}</p>
+                <p className="comment-body">{comment.body}</p>
+                <p className="comment-votes">Votes: {comment.votes}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
   );
 };
 
