b9d15f45e42210ac7fde821ecc8a4a41048e26b1
Peter Matthews <peterjmatthews@me.com>
2023-04-22 10:26:17 +0100

    Restructure card layout & add logic to crop titles

--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -6,16 +6,15 @@ const ReviewCard = ({ review }: { review: Review }): JSX.Element => {
 
   return (
     <li className="list-item">
-      <Link to={`/reviews/${review.review_id}`}>
-        <h3 className="list-item-header">{review.title.slice(0, 35)}</h3>
-      </Link>
-      <h5 className="list-item-designer">{review.designer}</h5>
-      <h5 className="list-item-owner">{review.owner}</h5>
       <img
         src={review.review_img_url}
         alt={`image for ${review.title}`}
         className="list-item-image"
       />
+      <Link to={`/reviews/${review.review_id}`}>
+        <h3 className="list-item-header">{review.title.length > 40 ? review.title.slice(0, 37) + "..." : review.title}</h3>
+      </Link>
+      <h5 className="list-item-owner">{review.owner}</h5>
       <p className="list-item-body">
         {review.review_body.slice(0, 200)} ...
         <Link className="read-more" to={`/reviews/${review.review_id}`}>read more</Link>
