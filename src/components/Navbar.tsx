2fb47793bc2454e9a85bcae6d30ecad8416dec33
Peter Matthews <peterjmatthews@me.com>
2023-05-23 11:10:51 +0100

    add functionality to toggle show/hide navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,31 @@
 import { Link } from "react-router-dom";
-import { FaBars, FaTimes} from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
 
 const Navbar = (): JSX.Element => {
+  const navRef = useRef<HTMLElement>(null);
+
+  const showNavBar = () => {
+    navRef.current?.classList.toggle("responsive_nav");
+  };
+
   return (
     <header>
       <Link to={`/reviews`}>
         <h3>House of Games</h3>
       </Link>
-      <Link to={`/reviews`}>
-        <a>Reviews</a>
-      </Link>
-      <Link to={`/users`}>
-        <a>Users</a>
-      </Link>
-      <button>
-        <FaTimes/>
-      </button>
-      <button>
+      <nav ref={navRef}>
+        <Link to={`/reviews`}>
+          <a>Reviews</a>
+        </Link>
+        <Link to={`/users`}>
+          <a>Users</a>
+        </Link>
+        <button onClick={showNavBar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button onClick={showNavBar}>
         <FaBars />
       </button>
     </header>
