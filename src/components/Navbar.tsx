9798b3f2faac6004643c02cc9136994cccf74628
Peter Matthews <peterjmatthews@me.com>
2023-05-25 17:48:55 +0100

    add styles to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from "react";
+import "../styles/Navbar.css"
 
 const Navbar = (): JSX.Element => {
   const navRef = useRef<HTMLElement>(null);
 
   const showNavBar = () => {
     navRef.current?.classList.toggle("responsive_nav");
+  };
+
+  const hideNavbar =()=>{
+    navRef.current?.classList.remove("responsive_nav")
   };
 
   return (
@@ -21,11 +26,11 @@ const Navbar = (): JSX.Element => {
         <Link to={`/users`}>
           <a>Users</a>
         </Link>
-        <button onClick={showNavBar}>
+        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
           <FaTimes />
         </button>
       </nav>
-      <button onClick={showNavBar}>
+      <button className="nav-btn" onClick={showNavBar}>
         <FaBars />
       </button>
     </header>
